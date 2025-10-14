ee9f576e39b1912ea9d189dd2275addb71807117
Prathmesh <prathmeshk1401@gmail.com>
2025-10-14 22:01:15 +0530

    Logo styles are changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-import "./Navbar.css"
-import verdure from "../../assets/VERDURE-logo.png"
-const Navbar = () => {
-    return (
-        <nav>
-            <div className='navContainer'>
-                
-                    <img src={verdure} alt="Logo" id="logo" className='logo' />
-                
-            </div>
-        </nav>
-    )
-}
+// import React from 'react'
+// import { Link } from "react-router-dom";
+// import "./Navbar.css"
+// import verdure from "../../assets/VERDURE-logo.png"
+// const Navbar = () => {
+//     return (
+//         <nav>
+//             <div className='navContainer'>
+//                     <img src={verdure} alt="Logo" id="logo" className='logo' />
+//             </div>
+//         </nav>
+//     )
+// }
 
-export default Navbar
+// export default Navbar
